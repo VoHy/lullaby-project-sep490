cf24e691f305fab2ca45c7dd10734e89c378b62d
Guin <vohyyy321@gmail.com>
2025-05-15 15:22:57 +0700

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/* eslint-disable @next/next/no-page-custom-font */
+module.exports = {
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      animation: {
+        'fade-in': 'fadeIn 0.5s ease-in forwards',
+      },
+      keyframes: {
+        fadeIn: {
+          '0%': { opacity: 0 },
+          '100%': { opacity: 1 },
+        },
+      },
+    }
+  },
+  plugins: [],
+}
+  
